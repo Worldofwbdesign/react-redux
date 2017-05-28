89735ab68175065d84114181399fd8b0c0aa6595
Worldofwbdesign <zavodzamutov@gmail.com>
2017-05-28 16:35:21 +0300

    add few redux features

--- app/redux-example.jsx
+++ app/redux-example.jsx
@@ -1,80 +1,24 @@
 let redux = require('redux');
+let axios = require('axios');
 
-let searchTextReducer = (state = '', action) => {
-  switch(action.type) {
-    case 'UPDATE_SEARCH_TEXT':
-      return action.searchText
-    default:
-      return state;
-  }
-};
-
-let updateSearchText = (searchText) => {
-  return {
-    type: 'UPDATE_SEARCH_TEXT',
-    searchText
-  }
-};
-
-
-let todoId = 1;
-let todosReducer = (state = [], action) => {
-  switch(action.type) {
-
-    case 'ADD_TODO':
-      return [
-          ...state,
-          {
-            id: todoId++,
-            title: action.title,
-            dateCreated: action.dateCreated,
-          }
-        ]
-      break;
-
-    case 'REMOVE_TODO':
-      return state.filter((todo) => todo.id !== action.id)
-
-    default:
-      return state;
-  }
-};
-
-let addTodo = (title, dateCreated) => {
-  return {
-    type: 'ADD_TODO',
-    title,
-    dateCreated
-  }
-};
-
-let removeTodo = (id) => {
-  return {
-    type: 'REMOVE_TODO',
-    id
-  }
-}
-
-let reducer = redux.combineReducers({
-  searchText: searchTextReducer,
-  todos: todosReducer
-})
-
-let store = redux.createStore(reducer, redux.compose(
-  window.devToolsExtension ? window.devToolsExtension() : f => f
-));
+let actions = require('./actions/index');
+let store = require('./store/configureStore').configure();
 
 let unsubscribe = store.subscribe(() => {
   let state = store.getState();
 
-  console.log(state.searchText);
-  document.getElementById('app').innerHTML = state.searchText;
+  if (state.map.isFetching) {
+    document.getElementById('app').innerHTML = 'Loading...';
+  } else if (state.map.url) {
+    document.getElementById('app').innerHTML = `<a target="_blank" href="${state.map.url}">Check out your location</a>`;
+  }
 
-  console.log('new todos array', store.getState());
+  console.log('new state', store.getState());
 });
 
-store.dispatch(updateSearchText('Dogs'));
-store.dispatch(updateSearchText('Cats'));
-store.dispatch(addTodo('Feed Cat', '14.05.2017'));
-store.dispatch(addTodo('Feed Dog', '14.06.2017'));
-store.dispatch(removeTodo(2));
+store.dispatch(actions.updateSearchText('Dogs'));
+store.dispatch(actions.updateSearchText('Cats'));
+store.dispatch(actions.addTodo('Feed Cat', '14.05.2017'));
+store.dispatch(actions.addTodo('Feed Dog', '14.06.2017'));
+store.dispatch(actions.removeTodo(2));
+store.dispatch(actions.fetchLocation());
